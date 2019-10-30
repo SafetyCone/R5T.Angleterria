a0cb49d55558144753b5bb64122500760971d8b8
MinexAutomation <minex.automation@gmail.com>
2019-10-29 22:29:13 -0700

    Changed the framework class to provide a framework name, not a directory name, duh!

--- source/R5T.Angleterria/Code/Classes/NetCoreAppV2_2.cs
+++ source/R5T.Angleterria/Code/Classes/NetCoreAppV2_2.cs
@@ -5,6 +5,6 @@ namespace R5T.Angleterria
 {
     public class NetCoreAppV2_2
     {
-        public const string DirectoryName = "netcoreapp2.2";
+        public const string FrameworkName = "netcoreapp2.2";
     }
 }
