d45447d421974d947767467348f10a5a38f0df8d
MinexAutomation <minex.automation@gmail.com>
2020-05-05 15:22:21 -0700

    Added BuildConfigurationName and TargetFrameworkName static classes. Updated project plan.

---
+++ source/R5T.Angleterria/Code/Classes/BuildConfigurationName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Angleterria
+{
+    public static class BuildConfigurationName
+    {
+        public const string Debug = "Debug";
+        public const string Release = "Release";
+    }
+}
---
+++ source/R5T.Angleterria/Code/Classes/TargetFrameworkName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Angleterria
+{
+    public static class TargetFrameworkName
+    {
+        public const string NetStandard2_0 = "netstandard2.0";
+
+        public const string NetCoreApp2_2 = "netcoreapp2.2";
+    }
+}
